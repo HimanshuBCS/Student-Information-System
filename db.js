2c00d3f0b7f20400e580f00a9a7d49a7cf1eae43
HimanshuP90 <himanshu.pandey@bridgeconn.com>
2018-01-04 16:03:28 +0530

    Student form validations added

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 var $ = document.querySelector.bind(document);
 
-var db = new PouchDB('Student');
+var db = new PouchDB('http://127.0.0.1:5984/Students');
+
+// var db = new PouchDB('Students');
 
 db.info().then(function (info) {
   console.log(info);
